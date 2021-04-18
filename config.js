5d2d9ffc22d6487271589fa49e002d78c3c3cccb
trevorspecht <trevor.specht@gmail.com>
2021-04-17 23:33:25 -0400

    replace config and app with spirits code

--- config.js
+++ config.js
@@ -1,29 +1,27 @@
 
 const config = {
     style: "mapbox://styles/mapbox/dark-v10",
-    accessToken: "pk.eyJ1IjoidHJldm9yc3BlY2h0IiwiYSI6ImNrYzJoM2t4ODAxNDAycnF0cHo5eHoybDcifQ.-Pw1y6ZbWuUMooRWmJAK1Q",
-    CSV: "https://docs.google.com/spreadsheets/d/1TLU9QKCgdmhtlme6szcqdyc7Y70-qJJsATEHwlQPv88/gviz/tq?tqx=out:csv&sheet=Sheet1",
-    center: [-76.610759, 39.2908816], //Lng, Lat
-    zoom: 11, //Default zoom
-    title: "Baltimore Black-owned Restaurants",
-    description: "You can search by cuisine to sort the list below by cuisine type.",
-    sideBarInfo: ["Name", "Address", "Website", "Phone"],
+    accessToken: "pk.eyJ1IjoidHJldm9yc3BlY2h0IiwiYSI6ImNrazNoazVvaDBpZmMycGszNWZkYWl0Y3QifQ.d2ogRic-OLtqdvBk0YZltA",
+    CSV: "https://docs.google.com/spreadsheets/d/1BIqmPT06veFDmiBFL-80DtxIr44tVr8xoXUE2_x0TE0/gviz/tq?tqx=out:csv&sheet=Sheet1",
+    center: [-100, 39], //Lng, Lat
+    zoom: 3, //Default zoom
+    title: "Black-owned Spirits",
+    description: "Click on a location to find available spirits",
+    sideBarInfo: ["Name", "Spirit", "Locations", "Description"],
     popupInfo: ["Name", "Website"],
     filters: [
         {
             type: "dropdown",
-            title: "Cuisines",
-            columnHeader: "Cuisine",
+            title: "Spirit Type",
+            columnHeader: "Spirit",
             listItems: [
-              'African/Caribbean',
-              'American',
-              'Asian',
-              'Breakfast Specialty',
-              'Seafood',
-              'Soul/Creole',
-              'Vegan/Healthy',
-              'Dessert',
-              'Coffee/Tea'
+              'Vodka',
+              'Bourbon/Whiskey',
+              'Tequila',
+              'Rum',
+              'Gin',
+              'Cognac',
+              'Liqueur/Other'
             ]
         }
       /*  {
